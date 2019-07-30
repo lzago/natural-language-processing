c26634c00c794c7f8eb656f196b278cdf125b3e9
lzago <joelfleish1973@gmail.com>
2019-07-30 11:14:01 +0200

    first commit

--- project/dialogue_manager.py
+++ project/dialogue_manager.py
@@ -44,6 +44,7 @@ class DialogueManager(object):
         # Goal-oriented part:
         self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
         self.thread_ranker = ThreadRanker(paths)
+        self.create_chitchat_bot()
 
     def create_chitchat_bot(self):
         """Initializes self.chitchat_bot with some conversational model."""
