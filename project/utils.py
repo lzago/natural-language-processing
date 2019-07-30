2987f03f8e3f4b5503fb16ecd512694bee5cd482
lzago <joelfleish1973@gmail.com>
2019-07-30 11:10:04 +0200

    first commit

--- project/utils.py
+++ project/utils.py
@@ -18,7 +18,7 @@ RESOURCE_PATH = {
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
-    
+
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
     bad_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
@@ -41,7 +41,7 @@ def load_embeddings(embeddings_path):
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
     """
-    
+
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
@@ -50,16 +50,18 @@ def load_embeddings(embeddings_path):
     #### YOUR CODE HERE ####
     ########################
 
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    embeddings = dict()
+    for line in open(embeddings_path, encoding='utf-8'):
+        row = line.strip().split('\t')
+        embeddings[row[0]] = np.array(row[1:], dtype=np.float32)
+    embeddings_dim = embeddings[list(embeddings)[0]].shape[0]
+
+    return embeddings, embeddings_dim
 
 
 def question_to_vec(question, embeddings, dim):
     """Transforms a string to an embedding by averaging word embeddings."""
-    
+
     # Hint: you have already implemented exactly this function in the 3rd assignment.
 
     ########################
@@ -67,10 +69,11 @@ def question_to_vec(question, embeddings, dim):
     ########################
 
     # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    question_embedding = [embeddings[word] for word in question.split() if word in embeddings]
+    if not question_embedding:
+        return np.zeros(dim)
+    question_embedding = np.array(question_embedding)
+    return question_embedding.mean(axis=0)
 
 
 def unpickle_file(filename):
